adcf38db536aec9bf6695020f6251c0f28c3eba2
PagotDFloW <webflopgt@gmail.com>
2021-10-06 18:17:47 +0200

    ajout inscription component  + bootstrap

---
+++ WikiJV/src/app/inscription/inscription.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+
+@Component({
+  selector: 'app-inscription',
+  templateUrl: './inscription.component.html',
+  styleUrls: ['./inscription.component.css']
+})
+export class InscriptionComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  onFormSubmit(userForm:NgForm){
+    console.log(userForm);
+  }
+  resetUserForm(userForm: NgForm){
+    userForm.resetForm();
+  }
+
+}
