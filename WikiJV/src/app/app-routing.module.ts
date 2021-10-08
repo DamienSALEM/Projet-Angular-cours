8c2b2bda122d70ec4d9a22284c8be49767a2e05a
Thilleli <64068415+Thilleli@users.noreply.github.com>
2021-10-08 15:52:35 +0200

    modifs root

--- WikiJV/src/app/app-routing.module.ts
+++ WikiJV/src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AboutComponent } from './about/about.component';
 import { AppComponent } from './app.component';
+import { ArticleComponent } from './article/article.component';
 import { ContactsComponent } from './contacts/contacts.component';
 import { HomeComponent } from './home/home.component';
 
@@ -10,7 +11,7 @@ const routes: Routes = [
   { path: "home", component: HomeComponent},
   { path: "about", component: AboutComponent},
   { path: "contacts", component: ContactsComponent},
-  
+  { path: "article", component: ArticleComponent},
 ];
 
 @NgModule({
@@ -18,3 +19,4 @@ const routes: Routes = [
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+
